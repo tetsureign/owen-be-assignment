1e2a208cf543fe48cae9f93a300b9c9f7ddf9105
Thu Nguyen <thuthien2001@gmail.com>
2025-10-05 17:52:10 +0700

    feat: remove unused files, updated config to read from .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
 import { AttachmentsModule } from './attachments/attachments.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST ?? 'localhost',
@@ -20,7 +22,5 @@ import { AttachmentsModule } from './attachments/attachments.module';
     ProductsModule,
     AttachmentsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
